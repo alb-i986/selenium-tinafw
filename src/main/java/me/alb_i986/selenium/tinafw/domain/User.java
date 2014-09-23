142158d8a4c12dd500871f1b5129559f451babc5
Alberto Scotto <scotto.alberto.86@gmail.com>
2014-09-23 01:09:50 +0100

    Refactoring User.
    - dropping the field role
    - adding the field password

--- src/main/java/me/alb_i986/selenium/tinafw/domain/User.java
+++ src/main/java/me/alb_i986/selenium/tinafw/domain/User.java
@@ -4,18 +4,18 @@ import me.alb_i986.selenium.tinafw.pages.HomePage;
 
 /**
  * A User has a Browser.
- * A User may have an email, a role.
+ * A User may also have an email, and a password.
  */
 public class User {
 
 	private Browser browser;
 	private String email = "";
-	private String role = "";
+	private String password = "";
 
 	/**
-	 * Create a User with nothing but a Browser.
+	 * Create a User with nothing but a (closed) Browser.
 	 * Then, you may set the other fields using
-	 * the with methods, e.g. {@link #withEmail(String)}.
+	 * the <i>with</i> methods, e.g. {@link #withEmail(String)}.
 	 */
 	public User() {
 		this.browser = new Browser();
@@ -28,6 +28,10 @@ public class User {
 
 	/**
 	 * Open the browser and browse to the homepge.
+	 * Please bear in mind that it is illegal to open
+	 * the browser more than once without closing it
+	 * first.
+	 * 
 	 * @return {@link HomePage}
 	 * 
 	 * @see Browser#open()
@@ -50,10 +54,6 @@ public class User {
 		return email;
 	}
 
-	public String getRole() {
-		return role;
-	}
-
 	public User withEmail(String email) {
 		if(email == null)
 			this.email = "";
@@ -61,12 +61,13 @@ public class User {
 			this.email = email;
 		return this;
 	}
-
-	public User withRole(String role) {
-		if(role == null)
-			this.role = "";
-		else
-			this.role = role;
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public User withPassword(String password) {
+		this.password = password;
 		return this;
 	}
 
