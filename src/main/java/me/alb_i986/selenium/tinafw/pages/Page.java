f86606586ea845eb407dde2279395a906296ec0b
Alberto Scotto <scotto.alberto.86@gmail.com>
2014-09-27 02:29:03 +0100

    improve logging: "loading page $PAGE_CLASS" every time a `new Page` is called

--- src/main/java/me/alb_i986/selenium/tinafw/pages/Page.java
+++ src/main/java/me/alb_i986/selenium/tinafw/pages/Page.java
@@ -43,6 +43,7 @@ public abstract class Page {
 		this.driver = driver;
 		this.previousPage = previous;
 		PageFactory.initElements(driver, this);
+		logger.info("Loading page " + getClass().getSimpleName());
 		waitUntilIsLoaded();
 	}
 	
