e1f01b20d344cd1980cec9240df2ae09d31d3e96
Alberto Scotto <scotto.alberto.86@gmail.com>
2014-10-27 16:35:07 +0000

    refactor JunitWebTest: mv TestWatcher to its own class TestLogger

--- src/main/java/me/alb_i986/selenium/tinafw/tests/JunitWebTest.java
+++ src/main/java/me/alb_i986/selenium/tinafw/tests/JunitWebTest.java
@@ -2,6 +2,7 @@ package me.alb_i986.selenium.tinafw.tests;
 
 import me.alb_i986.selenium.tinafw.tests.rules.BrowserManager;
 import me.alb_i986.selenium.tinafw.tests.rules.HtmlReporter;
+import me.alb_i986.selenium.tinafw.tests.rules.TestLogger;
 import me.alb_i986.selenium.tinafw.tests.rules.TestRetrier;
 import me.alb_i986.selenium.tinafw.utils.PropertyLoader;
 
@@ -9,11 +10,9 @@ import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
-import org.junit.internal.AssumptionViolatedException;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 import org.junit.rules.TestWatcher;
-import org.junit.runner.Description;
 
 /**
  * JunitWebTest provides a few useful JUnit Rules out of the box:
@@ -46,37 +45,12 @@ public abstract class JunitWebTest implements WebTest {
     protected TestRetrier retryRule = new TestRetrier(MAX_EXECUTIONS);
     protected BrowserManager browserManager = new BrowserManager();
     protected HtmlReporter htmlReporter = new HtmlReporter();
-	protected TestWatcher watchman = new TestWatcher() {
-		
-		@Override
-		protected void skipped(AssumptionViolatedException e, Description description) {
-			super.skipped(e, description);
-			logger.error("END test: SKIPPED > " + description.getDisplayName() + " -- cause: " + e);
-		}
-
-		@Override
-		protected void starting(Description description) {
-			super.starting(description);
-			logger.info("BEGIN test " + description.getDisplayName());
-		}
-
-		@Override
-		protected void failed(Throwable e, Description description) {
-			super.failed(e, description);
-			logger.error("END test: FAILED > " + description.getDisplayName());
-		}
-
-		@Override
-		protected void succeeded(Description description) {
-			super.succeeded(description);
-			logger.info("END test: PASSED > " + description.getDisplayName());
-		}
-	};
+	protected TestWatcher testLogger = new TestLogger();
 
 	@Rule
 	public TestRule ruleChain =
 		RuleChain
-			.outerRule(watchman)
+			.outerRule(testLogger)
 			.around(browserManager)
 			.around(htmlReporter)
 			.around(retryRule)
---
+++ src/main/java/me/alb_i986/selenium/tinafw/tests/rules/TestLogger.java
@@ -0,0 +1,49 @@
+package me.alb_i986.selenium.tinafw.tests.rules;
+
+import me.alb_i986.selenium.tinafw.tests.WebTest;
+
+import org.apache.log4j.Logger;
+import org.junit.internal.AssumptionViolatedException;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+/**
+ * Reports the status of the running test (starting/passed/failed/skipped) to a {@link Logger}.
+ * 
+ */
+public class TestLogger extends TestWatcher {
+	
+	protected static final Logger logger = Logger.getLogger(WebTest.class);
+	
+	@Override
+	protected void skipped(AssumptionViolatedException e, Description description) {
+		logger.info("END test: SKIPPED > " + description.getDisplayName() + " -- cause: " + e.getMessage());
+	}
+
+	@Override
+	protected void starting(Description description) {
+		super.starting(description);
+		logger.info("BEGIN test " + description.getDisplayName());
+	}
+
+	@Override
+	protected void failed(Throwable e, Description description) {
+		logFinishedTest(TestStatus.FAILED, description);
+	}
+
+	@Override
+	protected void succeeded(Description description) {
+		logFinishedTest(TestStatus.PASSED, description);
+	}
+	
+	private void logFinishedTest(TestStatus status, Description description) {
+		logger.info("END test: " + status + " > " + description.getDisplayName());
+	}
+	
+	enum TestStatus {
+		PASSED,
+		FAILED,
+		SKIPPED,
+	}
+
+}
