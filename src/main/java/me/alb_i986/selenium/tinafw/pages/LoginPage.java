cf86135701ea371f7e64a30739dc9c51a2c06250
Alberto Scotto <scotto.alberto.86@gmail.com>
2014-10-31 22:29:31 +0000

    LoginPage takes a generic parameter: the page that #loginAs will return

--- src/main/java/me/alb_i986/selenium/tinafw/pages/LoginPage.java
+++ src/main/java/me/alb_i986/selenium/tinafw/pages/LoginPage.java
@@ -1,17 +1,18 @@
 package me.alb_i986.selenium.tinafw.pages;
 
-
 /**
  * Represents the page for logging in which will typically offer
  * a form with two text fields, one for the username, and one
  * for the password.
+ *
+ * @param <T> the concrete Page that {@link #loginAs(String, String)} will return
  */
-public interface LoginPage extends LoadablePage {
+public interface LoginPage<T extends Page> extends LoadablePage {
 	
 	/**
 	 * Fill in the login form, and submit it.
 	 * @return the landing page after logging in
 	 */
-	public Page loginAs(String username, String password);
+	public T loginAs(String username, String password);
 
 }
