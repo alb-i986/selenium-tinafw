e1729d060e51be3e230b7b907f5f8afa0436b990
Alberto Scotto <scotto.alberto.86@gmail.com>
2014-11-02 23:16:55 +0000

    improve ImLoggedInBase

--- src/main/java/me/alb_i986/selenium/tinafw/tasks/ImLoggedInBase.java
+++ src/main/java/me/alb_i986/selenium/tinafw/tasks/ImLoggedInBase.java
@@ -2,22 +2,26 @@ package me.alb_i986.selenium.tinafw.tasks;
 
 import me.alb_i986.selenium.tinafw.pages.LoginPage;
 import me.alb_i986.selenium.tinafw.pages.Page;
-import me.alb_i986.selenium.tinafw.tasks.BaseWebTask;
 
-public abstract class ImLoggedInBase extends BaseWebTask {
+/**
+ * Concrete subclasses must define the method `getPageClassToLoad`
+ * which needs to return a concrete {@link LoginPage}.
+ *
+ */
+public abstract class ImLoggedInBase extends NavigationWebTask {
 
 	/**
 	 * Browse to the login page and do the login.
 	 * 
 	 * @param noPage this param won't be considered: may be null
 	 */
+	@SuppressWarnings("rawtypes")
 	public Page run(Page noPage) {
-		return
-			user.getBrowser().browseTo(getLoginPageClass())
+		Page loginPage = super.run(noPage);
+		return 
+			((LoginPage) loginPage)
 				.loginAs(user.getUsername(), user.getPassword())
 			;
 	}
 	
-	public abstract Class<? extends LoginPage> getLoginPageClass();
-
 }
