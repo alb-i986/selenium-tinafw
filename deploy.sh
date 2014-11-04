f9f448beec5a4cb7203a4bf635d7ea99311be417
Alberto Scotto <scotto.alberto.86@gmail.com>
2014-11-04 01:11:45 +0000

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,30 @@
 
 
 git co master
+
+# assert that the build is good
 mvn clean install
 if [ $? -ne 0 ] ; then
   exit $?
 fi
+
+# deploy sources to github
 git push origin master
 
-mvn javadoc:javadoc
+# deploy javadoc to github (gh-pages)
 rm -rf /tmp/apidocs
 mv target/apidocs/ /tmp/
-gs
 git co gh-pages
 git pull
 rm -rf javadoc/
 mv /tmp/apidocs/ javadoc
-gs
+git status
 git add --all javadoc/
 git commit -m "update javadoc"
 git push origin gh-pages
+git branch -d gh-pages
 
-# finally checkout master
+# finally checkout master and deploy to maven central
 git co master
+mvn deploy
 
