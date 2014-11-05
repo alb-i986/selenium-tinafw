be2975ee53ebc22efab9a254d2532136e08fb3cc
Alberto Scotto <scotto.alberto.86@gmail.com>
2014-11-05 21:40:43 +0000

    add NullTask

---
+++ src/main/java/me/alb_i986/selenium/tinafw/tasks/NullTask.java
@@ -0,0 +1,18 @@
+package me.alb_i986.selenium.tinafw.tasks;
+
+import me.alb_i986.selenium.tinafw.pages.Page;
+
+/**
+ * Task that does nothing.
+ */
+public class NullTask extends BaseWebTask {
+
+	/**
+	 * @return the very given page
+	 */
+	@Override
+	public Page run(Page initialPage) {
+		return initialPage;
+	}
+
+}
