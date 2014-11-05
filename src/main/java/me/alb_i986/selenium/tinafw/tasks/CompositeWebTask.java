143e626f0c9cda6ed54f434249189c280aac89d6
Alberto Scotto <scotto.alberto.86@gmail.com>
2014-11-05 23:11:33 +0000

    clean CompositeWebTask

--- src/main/java/me/alb_i986/selenium/tinafw/tasks/CompositeWebTask.java
+++ src/main/java/me/alb_i986/selenium/tinafw/tasks/CompositeWebTask.java
@@ -1,10 +1,10 @@
 package me.alb_i986.selenium.tinafw.tasks;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-
-import org.junit.Assert;
 
 import me.alb_i986.selenium.tinafw.pages.Page;
 
@@ -29,20 +29,19 @@ public class CompositeWebTask extends BaseWebTask implements Iterable<WebTask> {
 	private List<WebTask> subtasks = new ArrayList<>();
 	
 	/**
-	 * Add all the given subtasks to the list of components.
-	 * Discards any given null task, if any.
+	 * @see #CompositeWebTask(List)
 	 */
 	public CompositeWebTask(WebTask... subtasks) {
-		for (WebTask subtask : subtasks) {
-			if(subtask == null)
-				logger.debug("Building CompositeWebTask: discarding null task");
-			else
-				this.subtasks.add(subtask);
-		}
+		this(Arrays.asList(subtasks));
 	}
-	
+
+	/**
+	 * Set the given list as the list of components of this composite.
+     * Replace any null subtask with a {@link NullTask}.
+	 */
 	public CompositeWebTask(List<WebTask> subtasks) {
 		super();
+		Collections.replaceAll(subtasks, null, new NullTask());
 		this.subtasks = subtasks;
 	}
 
@@ -59,7 +58,6 @@ public class CompositeWebTask extends BaseWebTask implements Iterable<WebTask> {
 		for(WebTask task : this) {
 			logger.info("BEGIN subtask " + task.getClass().getSimpleName());
 			// before running the subtask, set the user
-			Assert.assertNotNull(getUser());
 			task.setUser(getUser());
 			currentPage = task.run(currentPage);
 			logger.info("END subtask " + task.getClass().getSimpleName());
