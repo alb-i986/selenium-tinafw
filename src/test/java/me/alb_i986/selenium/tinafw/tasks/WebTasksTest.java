a062c5240970edd68d0f1d9fd2cc40dc985e7f8b
Alberto Scotto <scotto.alberto.86@gmail.com>
2014-12-06 00:32:08 +0000

    update WebTasksTest: refactor + add test for #nullTask

--- src/test/java/me/alb_i986/selenium/tinafw/tasks/WebTasksTest.java
+++ src/test/java/me/alb_i986/selenium/tinafw/tasks/WebTasksTest.java
@@ -1,23 +1,19 @@
 package me.alb_i986.selenium.tinafw.tasks;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import me.alb_i986.selenium.tinafw.ui.WebPage;
-
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 public class WebTasksTest {
+
+	private WebPage stubPage;
 	
-	/**
-	 * Given an initial page,
-	 * When sleepFor 3 seconds,
-	 * Then the method should return after 3 seconds (let's say between 3 and 4 seconds),
-	 * And the returned page is the same object as the one passed.
-	 */
-	@Test
-	public void sleepFor() {
-		WebPage initialPage = new WebPage() {
+	@Before
+	public void before() {
+		stubPage = new WebPage() {
 			
 			@Override
 			public String getTitle() {
@@ -29,15 +25,41 @@ public class WebTasksTest {
 				return "current url";
 			}
 		};
+	}
+	
+
+	/**
+	 * Testing {@link WebTasks#nullTask()}.
+	 * <p>
+	 * Given an initial page,
+	 * When {@link WebTasks#nullTask()} is run,
+	 * Then the returned page should be the same as the initial page.
+	 */
+	@Test
+	public void nullTask() {
+		WebPage finalPage = WebTasks.nullTask().run(stubPage);
+		assertSame(stubPage, finalPage);
+	}
+	
+	/**
+	 * Testing {@link WebTasks#sleepFor(int)}.
+	 * <p>
+	 * Given an initial page,
+	 * When sleepFor 2 seconds,
+	 * Then the method should return after 2 seconds (let's say between 1 and 3 seconds),
+	 * And the returned page should be the same as the initial page.
+	 */
+	@Test
+	public void sleepFor() {
 		long startTimeMillis = System.currentTimeMillis();
-		WebPage finalPage = WebTasks.sleepFor(3).run(initialPage);
+		WebPage finalPage = WebTasks.sleepFor(2).run(stubPage);
 		long endTimeMillis = System.currentTimeMillis();
 		assertThat(endTimeMillis - startTimeMillis,
 				allOf(
-					greaterThanOrEqualTo(3000L),
-					lessThanOrEqualTo(4000l)
+					greaterThanOrEqualTo(1000L),
+					lessThanOrEqualTo(3000L)
 				)
 		);
-		assertSame(initialPage, finalPage);
+		assertSame(stubPage, finalPage);
 	}
 }
